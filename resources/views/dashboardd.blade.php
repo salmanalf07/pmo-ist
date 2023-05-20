fccf8aef9c3f6840a144a10fdd62f2081c59afd7
salmanalf07 <salmanalf07@gmail.com>
2023-05-20 16:21:44 +0700

    dashboard & login

--- resources/views/dashboardd.blade.php
+++ resources/views/dashboardd.blade.php
@@ -1113,12 +1113,15 @@
                                         </a>
                                     </li>
                                     <li>
-                                        <a class="dropdown-item" href="./index.html">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-power me-2 icon-xxs dropdown-item-icon">
-                                                <path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path>
-                                                <line x1="12" y1="2" x2="12" y2="12"></line>
-                                            </svg>Sign Out
-                                        </a>
+                                        <form method="POST" action="{{ route('logout') }}">
+                                            @csrf
+                                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit(); " role="button">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-power me-2 icon-xxs dropdown-item-icon">
+                                                    <path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path>
+                                                    <line x1="12" y1="2" x2="12" y2="12"></line>
+                                                </svg>Sign Out
+                                            </a>
+                                        </form>
                                     </li>
                                 </ul>
 
