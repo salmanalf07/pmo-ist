236386584b9ab8e884cc83b2ca34208f445b458a
salmanalf07 <salmanalf07@gmail.com>
2023-05-31 19:04:32 +0700

    ganti afte login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/profile';
+    public const HOME = '/projectInfo';
 
     /**
      * The controller namespace for the application.
