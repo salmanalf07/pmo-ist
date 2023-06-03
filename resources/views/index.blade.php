4aecd1b08fbb67ca889ca7e6c1f351125c2064cb
salmanalf07 <salmanalf07@gmail.com>
2023-06-04 06:32:12 +0700

    edit index.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,7 +47,6 @@
     <script src="/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="/assets/libs/feather-icons/dist/feather.min.js"></script>
     <script src="/assets/libs/simplebar/dist/simplebar.min.js"></script>
-    <script src="/assets/js/vendors/tooltip.js"></script>
     <!-- flatpickr -->
     <script src="/assets/libs/flatpickr/dist/flatpickr.min.js"></script>
     <!-- quill js -->
