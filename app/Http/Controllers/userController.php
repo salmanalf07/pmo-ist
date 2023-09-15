6ce35e6c3132b2571861361ab6d65022f2f93625
salmanalf07 <salmanalf07@gmail.com>
2023-09-16 05:55:41 +0700

    edit uniqe user

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -31,7 +31,7 @@ class userController extends Controller
         try {
             $request->validate([
                 'name' => ['required', 'string', 'max:255', 'not_equal_to:#'],
-                'username' => ['required', 'string', 'max:255'],
+                'username' => ['required', 'string', 'max:255', 'unique:users'],
                 'password' => ['required', 'digits:8'],
                 'role.*' => ['required', 'string'],
                 'role' => ['required', 'array'],
