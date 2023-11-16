0b9ee448ef902f6d6be892832adbb500e06345df
salmanalf07 <salmanalf07@gmail.com>
2023-11-16 19:18:43 +0700

    edit

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -46,8 +46,6 @@
                                             </span>
                                             {{$pmName}} <a style="margin-left: 0.5em;" href="#" id="detailModals" data-id="{{$projects[0]->pm?$projects[0]->pm->id:'#'}}" data-bs-toggle="tooltip" data-placement="top" title="Detail Data"><i class="bi bi-info-circle"></i>
                                         </td>
-                                        <td class="text-center">{{count($projects)}}</td>
-                                        <td>{{number_format((int)$projects->sum('projectValue'), 0, '', '.')}}</td>
                                     </tr>
                                     @endforeach
                                 </tbody>
