123411449957971f07d343a649ebabdbbba32fcc
salmanalf07 <salmanalf07@gmail.com>
2024-02-17 17:13:29 +0700

    edit model project sponsor

--- app/Models/projectSponsor.php
+++ app/Models/projectSponsor.php
@@ -4,12 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class projectSponsor extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'projectId',
