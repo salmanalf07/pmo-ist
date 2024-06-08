4edd75db51b0015c23d58f46722de9c527c4d82a
salmanalf07 <salmanalf07@gmail.com>
2024-06-08 16:10:29 +0700

    update command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Artisan;
 
 class Kernel extends ConsoleKernel
 {
@@ -16,7 +17,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('queue:work --tries=3 --timeout=2000')->withoutOverlapping();
+        $schedule->command('queue:work --tries=3 --timeout=2000')->withoutOverlapping()->after(function () {
+            // Clear the cache after queue:work finishes
+            Artisan::call('cache:clear');
+        });;
     }
 
     /**
