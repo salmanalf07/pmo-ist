ceda6b597c62bec72c4c1125ab4c0fedac2881c3
salmanalf07 <salmanalf07@gmail.com>
2024-06-24 04:56:41 +0700

    add status in by asana

--- resources/views/employee/byAsana.blade.php
+++ resources/views/employee/byAsana.blade.php
@@ -2,6 +2,11 @@
 
 @section('konten')
 <link href="/assets/css/select2Custom.css" rel="stylesheet">
+<style>
+    .icon-green path {
+        fill: green;
+    }
+</style>
 <div id="app-content">
     <!-- Container fluid -->
     <div class="app-content-area">
@@ -271,11 +276,16 @@
             ],
             "autoWidth": false,
             "columnDefs": [{
-                targets: [5, 6],
-                render: function(oTable) {
-                    return moment(oTable).format('DD-MM-YYYY');
+                    targets: [5, 6],
+                    render: function(oTable) {
+                        return moment(oTable).format('DD-MM-YYYY');
+                    }
+                },
+                {
+                    targets: [7],
+                    className: 'text-center'
                 }
-            }, ],
+            ],
             ajax: {
                 url: '/json_ByAsana',
                 data: function(d) {
@@ -314,6 +324,17 @@
                 {
                     data: 'due_on',
                     title: 'End Date'
+                },
+                {
+                    data: 'status',
+                    title: 'Status',
+                    render: function(data, type, row) {
+                        var icon = '';
+                        if (data == 1) {
+                            icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle icon-green" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/></svg>';
+                        }
+                        return icon;
+                    }
                 },
             ],
         });
